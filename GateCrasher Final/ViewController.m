184b98fb9fc1fbaeac6db319aa43d91cb74699a4
Ruan Lopes <Ruan@Ruans-MacBook-Pro.local>
2015-02-13 09:28:21 -0500

    Initial Commit

---
+++ GateCrasher Final/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GateCrasher Final
+//
+//  Created by Ruan Lopes on 2015-02-13.
+//  Copyright (c) 2015 Ruan Lopes. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ GateCrasher Final/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GateCrasher Final
+//
+//  Created by Ruan Lopes on 2015-02-13.
+//  Copyright (c) 2015 Ruan Lopes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
